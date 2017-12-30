23df7a3730ae50ccf70032f9a6311ad56f0659b7
Sanket Gujarathi <sanketgujarathi@gmail.com>
2017-12-30 23:47:25 +0530

    First commit of the a working quiz app built using angular and material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { QuestionComponent } from './question/question.component';
+import { AppRoutingModule } from './app-routing.module';
+import { QuestionService } from './question.service';
+import { ResultsComponent } from './results/results.component';
+import { ResultsService } from './results.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    QuestionComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [QuestionService, ResultsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
